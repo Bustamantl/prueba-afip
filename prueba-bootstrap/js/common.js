ea5472c1089b495f8feaeb0b992d77f4c5333633
leandro <99197923+Bustamantl@users.noreply.github.com>
2022-03-21 15:27:15 -0300

    GOO

---
+++ prueba-bootstrap/js/common.js
@@ -0,0 +1,426 @@
+$(function() {
+
+    var pathName = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1).toLowerCase();
+    seleccionar("menuLateral", pathName);
+    seleccionar("menuLateral2", pathName);
+
+    $('input[type=submit].fa-external-link').removeClass('fa-external-link');
+
+    $('.soloNumeros').keypress(function(e) { isNumber(e); });
+    $('.soloNumeros').keyup(function(e) {
+        if (/\D/g.test(this.value)) {
+            this.value = this.value.replace(/\D/g, '');
+        }
+    });
+});
+
+function seleccionar(menu, pathName) {
+    $("#" + menu + " li").removeClass("active");
+    for (var i = 0; i < $("#" + menu + " li").length; i++) {
+        if ($($("#" + menu + " li a")[i]).attr("href").toLowerCase() == pathName) {
+            $($("#" + menu + " li")[i]).addClass("active");
+        } else if ($($("#" + menu + " li a")[i]).attr("otraspaginas") != undefined) {
+            var paginas = $($("#" + menu + " li a")[i]).attr("otraspaginas").split(",");
+            for (var j = 0; j < paginas.length; j++) {
+                if (paginas[j].toLowerCase() == pathName) {
+                    $($("#" + menu + " li")[i]).addClass("active");
+                }
+            }
+        }
+    }
+}
+
+function scrollTo(element) {
+    $('html, body').animate({
+        scrollTop: $("#" + element).offset().top - 160
+    }, 'slow');
+}
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode >= 48 && charCode <= 57) {
+        return true;
+    }
+    return false;
+}
+
+function mostrarError(div, mensaje, hide) {
+    mensaje = mensaje.replace(/\h3/g, 'p');
+    $("#" + div).html(mensaje).removeClass("hide");
+    scrollTo(div);
+}
+
+// jQuery.toJSON
+(function($) {
+    'use strict';
+
+    var escape = /["\\\x00-\x1f\x7f-\x9f]/g,
+        meta = {
+            '\b': '\\b',
+            '\t': '\\t',
+            '\n': '\\n',
+            '\f': '\\f',
+            '\r': '\\r',
+            '"': '\\"',
+            '\\': '\\\\'
+        },
+        hasOwn = Object.prototype.hasOwnProperty;
+
+    /**
+     * jQuery.toJSON
+     * Converts the given argument into a JSON representation.
+     *
+     * @param o {Mixed} The json-serializable *thing* to be converted
+     *
+     * If an object has a toJSON prototype, that will be used to get the representation.
+     * Non-integer/string keys are skipped in the object, as are keys that point to a
+     * function.
+     *
+     */
+    $.toJSON = typeof JSON === 'object' && JSON.stringify ? JSON.stringify : function(o) {
+        if (o === null) {
+            return 'null';
+        }
+
+        var pairs, k, name, val,
+            type = $.type(o);
+
+        if (type === 'undefined') {
+            return undefined;
+        }
+
+        // Also covers instantiated Number and Boolean objects,
+        // which are typeof 'object' but thanks to $.type, we
+        // catch them here. I don't know whether it is right
+        // or wrong that instantiated primitives are not
+        // exported to JSON as an {"object":..}.
+        // We choose this path because that's what the browsers did.
+        if (type === 'number' || type === 'boolean') {
+            return String(o);
+        }
+        if (type === 'string') {
+            return $.quoteString(o);
+        }
+        if (typeof o.toJSON === 'function') {
+            return $.toJSON(o.toJSON());
+        }
+        if (type === 'date') {
+            var month = o.getUTCMonth() + 1,
+                day = o.getUTCDate(),
+                year = o.getUTCFullYear(),
+                hours = o.getUTCHours(),
+                minutes = o.getUTCMinutes(),
+                seconds = o.getUTCSeconds(),
+                milli = o.getUTCMilliseconds();
+
+            if (month < 10) {
+                month = '0' + month;
+            }
+            if (day < 10) {
+                day = '0' + day;
+            }
+            if (hours < 10) {
+                hours = '0' + hours;
+            }
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+            if (milli < 100) {
+                milli = '0' + milli;
+            }
+            if (milli < 10) {
+                milli = '0' + milli;
+            }
+            return '"' + year + '-' + month + '-' + day + 'T' +
+                hours + ':' + minutes + ':' + seconds +
+                '.' + milli + 'Z"';
+        }
+
+        pairs = [];
+
+        if ($.isArray(o)) {
+            for (k = 0; k < o.length; k++) {
+                pairs.push($.toJSON(o[k]) || 'null');
+            }
+            return '[' + pairs.join(',') + ']';
+        }
+
+        // Any other object (plain object, RegExp, ..)
+        // Need to do typeof instead of $.type, because we also
+        // want to catch non-plain objects.
+        if (typeof o === 'object') {
+            for (k in o) {
+                // Only include own properties,
+                // Filter out inherited prototypes
+                if (hasOwn.call(o, k)) {
+                    // Keys must be numerical or string. Skip others
+                    type = typeof k;
+                    if (type === 'number') {
+                        name = '"' + k + '"';
+                    } else if (type === 'string') {
+                        name = $.quoteString(k);
+                    } else {
+                        continue;
+                    }
+                    type = typeof o[k];
+
+                    // Invalid values like these return undefined
+                    // from toJSON, however those object members
+                    // shouldn't be included in the JSON string at all.
+                    if (type !== 'function' && type !== 'undefined') {
+                        val = $.toJSON(o[k]);
+                        pairs.push(name + ':' + val);
+                    }
+                }
+            }
+            return '{' + pairs.join(',') + '}';
+        }
+    };
+
+    /**
+     * jQuery.evalJSON
+     * Evaluates a given json string.
+     *
+     * @param str {String}
+     */
+    $.evalJSON = typeof JSON === 'object' && JSON.parse ? JSON.parse : function(str) {
+        /*jshint evil: true */
+        return eval('(' + str + ')');
+    };
+
+    /**
+     * jQuery.secureEvalJSON
+     * Evals JSON in a way that is *more* secure.
+     *
+     * @param str {String}
+     */
+    $.secureEvalJSON = typeof JSON === 'object' && JSON.parse ? JSON.parse : function(str) {
+        var filtered =
+            str
+            .replace(/\\["\\\/bfnrtu]/g, '@')
+            .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+            .replace(/(?:^|:|,)(?:\s*\[)+/g, '');
+
+        if (/^[\],:{}\s]*$/.test(filtered)) {
+            /*jshint evil: true */
+            return eval('(' + str + ')');
+        }
+        throw new SyntaxError('Error parsing JSON, source is not valid.');
+    };
+
+    /**
+     * jQuery.quoteString
+     * Returns a string-repr of a string, escaping quotes intelligently.
+     * Mostly a support function for toJSON.
+     * Examples:
+     * >>> jQuery.quoteString('apple')
+     * "apple"
+     *
+     * >>> jQuery.quoteString('"Where are we going?", she asked.')
+     * "\"Where are we going?\", she asked."
+     */
+    $.quoteString = function(str) {
+        if (str.match(escape)) {
+            return '"' + str.replace(escape, function(a) {
+                var c = meta[a];
+                if (typeof c === 'string') {
+                    return c;
+                }
+                c = a.charCodeAt();
+                return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+            }) + '"';
+        }
+        return '"' + str + '"';
+    };
+
+}(jQuery));
+
+// jQuery.visible
+// determina si un objeto est� visible en la pantalla dentro del scroll actual
+(function($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *       the user visible viewport of a web browser.
+     *       only accounts for vertical position, not horizontal.
+     */
+    var $w = $(window);
+    $.fn.visible = function(partial, hidden, direction) {
+
+        if (this.length < 1)
+            return;
+
+        var $t = this.length > 1 ? this.eq(0) : this,
+            t = $t.get(0),
+            vpWidth = $w.width(),
+            vpHeight = $w.height(),
+            direction = (direction) ? direction : 'both',
+            clientSize = hidden === true ? t.offsetWidth * t.offsetHeight : true;
+
+        if (typeof t.getBoundingClientRect === 'function') {
+
+            // Use this native browser method, if available.
+            var rec = t.getBoundingClientRect(),
+                tViz = rec.top >= 0 && rec.top < vpHeight,
+                bViz = rec.bottom > 0 && rec.bottom <= vpHeight,
+                lViz = rec.left >= 0 && rec.left < vpWidth,
+                rViz = rec.right > 0 && rec.right <= vpWidth,
+                vVisible = partial ? tViz || bViz : tViz && bViz,
+                hVisible = partial ? lViz || rViz : lViz && rViz;
+
+            if (direction === 'both')
+                return clientSize && vVisible && hVisible;
+            else if (direction === 'vertical')
+                return clientSize && vVisible;
+            else if (direction === 'horizontal')
+                return clientSize && hVisible;
+        } else {
+
+            var viewTop = $w.scrollTop(),
+                viewBottom = viewTop + vpHeight,
+                viewLeft = $w.scrollLeft(),
+                viewRight = viewLeft + vpWidth,
+                offset = $t.offset(),
+                _top = offset.top,
+                _bottom = _top + $t.height(),
+                _left = offset.left,
+                _right = _left + $t.width(),
+                compareTop = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom,
+                compareLeft = partial === true ? _right : _left,
+                compareRight = partial === true ? _left : _right;
+
+            if (direction === 'both')
+                return !!clientSize && ((compareBottom <= viewBottom) && (compareTop >= viewTop)) && ((compareRight <= viewRight) && (compareLeft >= viewLeft));
+            else if (direction === 'vertical')
+                return !!clientSize && ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+            else if (direction === 'horizontal')
+                return !!clientSize && ((compareRight <= viewRight) && (compareLeft >= viewLeft));
+        }
+    };
+
+})(jQuery);
+
+
+(function($) {
+
+    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+        a256 = '',
+        r64 = [256],
+        r256 = [256],
+        i = 0;
+
+    var UTF8 = {
+
+        /**
+         * Encode multi-byte Unicode string into utf-8 multiple single-byte characters
+         * (BMP / basic multilingual plane only)
+         *
+         * Chars in range U+0080 - U+07FF are encoded in 2 chars, U+0800 - U+FFFF in 3 chars
+         *
+         * @param {String} strUni Unicode string to be encoded as UTF-8
+         * @returns {String} encoded string
+         */
+        encode: function(strUni) {
+            // use regular expressions & String.replace callback function for better efficiency
+            // than procedural approaches
+            var strUtf = strUni.replace(/[\u0080-\u07ff]/g, // U+0080 - U+07FF => 2 bytes 110yyyyy, 10zzzzzz
+                    function(c) {
+                        var cc = c.charCodeAt(0);
+                        return String.fromCharCode(0xc0 | cc >> 6, 0x80 | cc & 0x3f);
+                    })
+                .replace(/[\u0800-\uffff]/g, // U+0800 - U+FFFF => 3 bytes 1110xxxx, 10yyyyyy, 10zzzzzz
+                    function(c) {
+                        var cc = c.charCodeAt(0);
+                        return String.fromCharCode(0xe0 | cc >> 12, 0x80 | cc >> 6 & 0x3F, 0x80 | cc & 0x3f);
+                    });
+            return strUtf;
+        },
+
+        /**
+         * Decode utf-8 encoded string back into multi-byte Unicode characters
+         *
+         * @param {String} strUtf UTF-8 string to be decoded back to Unicode
+         * @returns {String} decoded string
+         */
+        decode: function(strUtf) {
+            // note: decode 3-byte chars first as decoded 2-byte strings could appear to be 3-byte char!
+            var strUni = strUtf.replace(/[\u00e0-\u00ef][\u0080-\u00bf][\u0080-\u00bf]/g, // 3-byte chars
+                    function(c) { // (note parentheses for precence)
+                        var cc = ((c.charCodeAt(0) & 0x0f) << 12) | ((c.charCodeAt(1) & 0x3f) << 6) | (c.charCodeAt(2) & 0x3f);
+                        return String.fromCharCode(cc);
+                    })
+                .replace(/[\u00c0-\u00df][\u0080-\u00bf]/g, // 2-byte chars
+                    function(c) { // (note parentheses for precence)
+                        var cc = (c.charCodeAt(0) & 0x1f) << 6 | c.charCodeAt(1) & 0x3f;
+                        return String.fromCharCode(cc);
+                    });
+            return strUni;
+        }
+    };
+
+    while (i < 256) {
+        var c = String.fromCharCode(i);
+        a256 += c;
+        r256[i] = i;
+        r64[i] = b64.indexOf(c);
+        ++i;
+    }
+
+    function code(s, discard, alpha, beta, w1, w2) {
+        s = String(s);
+        var buffer = 0,
+            i = 0,
+            length = s.length,
+            result = '',
+            bitsInBuffer = 0;
+
+        while (i < length) {
+            var c = s.charCodeAt(i);
+            c = c < 256 ? alpha[c] : -1;
+
+            buffer = (buffer << w1) + c;
+            bitsInBuffer += w1;
+
+            while (bitsInBuffer >= w2) {
+                bitsInBuffer -= w2;
+                var tmp = buffer >> bitsInBuffer;
+                result += beta.charAt(tmp);
+                buffer ^= tmp << bitsInBuffer;
+            }
+            ++i;
+        }
+        if (!discard && bitsInBuffer > 0) result += beta.charAt(buffer << (w2 - bitsInBuffer));
+        return result;
+    }
+
+    var Plugin = $.base64 = function(dir, input, encode) {
+        return input ? Plugin[dir](input, encode) : dir ? null : this;
+    };
+
+    Plugin.btoa = Plugin.encode = function(plain, utf8encode) {
+        plain = Plugin.raw === false || Plugin.utf8encode || utf8encode ? UTF8.encode(plain) : plain;
+        plain = code(plain, false, r256, b64, 8, 6);
+        return plain + '===='.slice((plain.length % 4) || 4);
+    };
+
+    Plugin.atob = Plugin.decode = function(coded, utf8decode) {
+        coded = String(coded).split('=');
+        var i = coded.length;
+        do {
+            --i;
+            coded[i] = code(coded[i], true, r64, a256, 6, 8);
+        } while (i > 0);
+        coded = coded.join('');
+        return Plugin.raw === false || Plugin.utf8decode || utf8decode ? UTF8.decode(coded) : coded;
+    };
+}(jQuery));
